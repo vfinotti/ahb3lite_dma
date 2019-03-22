557f834fbb367061c99932adbbae5fe3d0f73f96
Vitor Finotti <vfinotti@ncc.unesp.br>
2019-03-22 14:47:59 -0300

    Adding body of code and connections

--- ahb3lite_dma.sv
+++ ahb3lite_dma.sv
@@ -82,54 +82,62 @@ module ahb3lite_dma #(
   // --------------------------------------
   // WISHBONE INTERFACE 0
   // Slave Interface
-  input  [31:0]         wb0s_data_i,
-  output [31:0]         wb0s_data_o,
-  input  [31:0]         wb0_addr_i,
-  input  [ 3:0]         wb0_sel_i,
-  input                 wb0_we_i,
-  input                 wb0_cyc_i,
-  input                 wb0_stb_i,
-  output                wb0_ack_o,
-  output                wb0_err_o,
-  output                wb0_rty_o,
+  input                 s0HSEL,
+  input  [31:0]         s0HADDR,
+  input  [31:0]         s0HWDATA,
+  output [31:0]         s0HRDATA,
+  input                 s0HWRITE,
+  input  [ 2:0]         s0HSIZE,
+  input  [ 2:0]         s0HBURST,
+  input  [ 3:0]         s0HPROT,
+  input  [ 1:0]         s0HTRANS,
+  output                s0HREADYOUT,
+  input                 s0HREADY,
+  output                s0HRESP,
 
   // Master Interface
-  input  [31:0]         wb0m_data_i,
-  output [31:0]         wb0m_data_o,
-  output [31:0]         wb0_addr_o,
-  output [ 3:0]         wb0_sel_o,
-  output                wb0_we_o,
-  output                wb0_cyc_o,
-  output                wb0_stb_o,
-  input                 wb0_ack_i,
-  input                 wb0_err_i,
-  input                 wb0_rty_i,
+  output                s0HSEL,
+  output [31:0]         s0HADDR,
+  output [31:0]         s0HWDATA,
+  input  [31:0]         s0HRDATA,
+  output                s0HWRITE,
+  output [ 2:0]         s0HSIZE,
+  output [ 2:0]         s0HBURST,
+  output [ 3:0]         s0HPROT,
+  output [ 1:0]         s0HTRANS,
+  input                 s0HREADYOUT,
+  output                s0HREADY,
+  input                 s0HRESP,
 
   // --------------------------------------
   // WISHBONE INTERFACE 1
   // Slave Interface
-  input  [31:0]         wb1s_data_i,
-  output [31:0]         wb1s_data_o,
-  input  [31:0]         wb1_addr_i,
-  input  [ 3:0]         wb1_sel_i,
-  input                 wb1_we_i,
-  input                 wb1_cyc_i,
-  input                 wb1_stb_i,
-  output                wb1_ack_o,
-  output                wb1_err_o,
-  output                wb1_rty_o,
+  input                 s1HSEL,
+  input  [31:0]         s1HADDR,
+  input  [31:0]         s1HWDATA,
+  output [31:0]         s1HRDATA,
+  input                 s1HWRITE,
+  input  [ 2:0]         s1HSIZE,
+  input  [ 2:0]         s1HBURST,
+  input  [ 3:0]         s1HPROT,
+  input  [ 1:0]         s1HTRANS,
+  output                s1HREADYOUT,
+  input                 s1HREADY,
+  output                s1HRESP,
 
   // Master Interface
-  input  [31:0]         wb1m_data_i,
-  output [31:0]         wb1m_data_o,
-  output [31:0]         wb1_addr_o,
-  output [ 3:0]         wb1_sel_o,
-  output                wb1_we_o,
-  output                wb1_cyc_o,
-  output                wb1_stb_o,
-  input                 wb1_ack_i,
-  input                 wb1_err_i,
-  input                 wb1_rty_i,
+  output                s1HSEL,
+  output [31:0]         s1HADDR,
+  output [31:0]         s1HWDATA,
+  input  [31:0]         s1HRDATA,
+  output                s1HWRITE,
+  output [ 2:0]         s1HSIZE,
+  output [ 2:0]         s1HBURST,
+  output [ 3:0]         s1HPROT,
+  output [ 1:0]         s1HTRANS,
+  input                 s1HREADYOUT,
+  output                s1HREADY,
+  input                 s1HRESP,
 
   // --------------------------------------
   // Misc Signal,
@@ -156,3 +164,230 @@ module ahb3lite_dma #(
   //
   // Module Body
   //
+  ahb3lite_to_wb_0 ahb3lite_to_wb (
+    .clk_i         ( clk_i          ),
+    .rst_n_i       ( rst_n_i        ),
+
+    // ahb3lite
+    .sHADDR        ( s0HADDR        ),
+    .sHWDATA       ( s0HWDATA       ),
+    .sHWRITE       ( s0HWRITE       ),
+    .sHREADYOUT    ( s0HREADYOUT    ),
+    .sHSIZE        ( s0HSIZE        ),
+    .sHBURST       ( s0HBURST       ),
+    .sHSEL         ( s0HSEL         ),
+    .sHTRANS       ( s0HTRANS       ),
+    .sHRDATA       ( s0HRDATA       ),
+    .sHRESP        ( s0HRESP        ),
+    .sHREADY       ( s0HREADY       ),
+    .sHPROT        ( s0HPROT        ),
+
+    // to wishbone
+    .to_wb_dat_i   ( to_wb0_dat_i   ),
+    .to_wb_adr_i   ( to_wb0_adr_i   ),
+    .to_wb_sel_i   ( to_wb0_sel_i   ),
+    .to_wb_we_i    ( to_wb0_we_i    ),
+    .to_wb_cyc_i   ( to_wb0_cyc_i   ),
+    .to_wb_stb_i   ( to_wb0_stb_i   ),
+    .from_wb_dat_o ( from_wb0_dat_o ),
+    .from_wb_ack_o ( from_wb0_ack_o ),
+    .from_wb_err_o ( from_wb0_err_o ) );
+
+
+  wb_to_ahb3lite_0 wb_to_ahb3lite (
+    .clk_i           ( clk_i            ),
+    .rst_n_i         ( rst_n_i          ),
+
+    //// wishbone
+    .from_m_wb_adr_o ( from_m_wb0_adr_o ),
+    .from_m_wb_sel_o ( from_m_wb0_sel_o ),
+    .from_m_wb_we_o  ( from_m_wb0_we_o  ),
+    .from_m_wb_dat_o ( from_m_wb0_dat_o ),
+    .from_m_wb_cyc_o ( from_m_wb0_cyc_o ),
+    .from_m_wb_stb_o ( from_m_wb0_stb_o ),
+    .to_m_wb_ack_i   ( to_m_wb0_ack_i   ),
+    .to_m_wb_err_i   ( to_m_wb0_err_i   ),
+    .to_m_wb_dat_i   ( to_m_wb0_dat_i   ),
+
+    .from_m_wb_cti_o ( from_m_wb0_cti_o ),
+    .from_m_wb_bte_o ( from_m_wb0_bte_o ),
+
+    //// to ahb3lite
+    .mHSEL           ( m0HSEL           ),
+    .mHSIZE          ( m0HSIZE          ),
+    .mHRDATA         ( m0HRDATA         ),
+    .mHRESP          ( m0HRESP          ),
+    .mHREADY         ( m0HREADY         ),
+    .mHREADYOUT      ( m0HREADYOUT      ),
+    .mHWRITE         ( m0HWRITE         ),
+    .mHBURST         ( m0HBURST         ),
+    .mHADDR          ( m0HADDR          ),
+    .mHTRANS         ( m0HTRANS         ),
+    .mHWDATA         ( m0HWDATA         ),
+    .mHPROT          ( m0HPROT          ) );
+
+
+  ahb3lite_to_wb_1 ahb3lite_to_wb (
+    .clk_i         ( clk_i          ),
+    .rst_n_i       ( rst_n_i        ),
+
+    // ahb3lite
+    .sHADDR        ( s1HADDR        ),
+    .sHWDATA       ( s1HWDATA       ),
+    .sHWRITE       ( s1HWRITE       ),
+    .sHREADYOUT    ( s1HREADYOUT    ),
+    .sHSIZE        ( s1HSIZE        ),
+    .sHBURST       ( s1HBURST       ),
+    .sHSEL         ( s1HSEL         ),
+    .sHTRANS       ( s1HTRANS       ),
+    .sHRDATA       ( s1HRDATA       ),
+    .sHRESP        ( s1HRESP        ),
+    .sHREADY       ( s1HREADY       ),
+    .sHPROT        ( s1HPROT        ),
+
+    // to wishbone
+    .to_wb_dat_i   ( to_wb1_dat_i   ),
+    .to_wb_adr_i   ( to_wb1_adr_i   ),
+    .to_wb_sel_i   ( to_wb1_sel_i   ),
+    .to_wb_we_i    ( to_wb1_we_i    ),
+    .to_wb_cyc_i   ( to_wb1_cyc_i   ),
+    .to_wb_stb_i   ( to_wb1_stb_i   ),
+    .from_wb_dat_o ( from_wb1_dat_o ),
+    .from_wb_ack_o ( from_wb1_ack_o ),
+    .from_wb_err_o ( from_wb1_err_o ) );
+
+
+  wb_to_ahb3lite_1 wb_to_ahb3lite (
+    .clk_i           ( clk_i            ),
+    .rst_n_i         ( rst_n_i          ),
+
+    //// wishbone
+    .from_m_wb_adr_o ( from_m_wb1_adr_o ),
+    .from_m_wb_sel_o ( from_m_wb1_sel_o ),
+    .from_m_wb_we_o  ( from_m_wb1_we_o  ),
+    .from_m_wb_dat_o ( from_m_wb1_dat_o ),
+    .from_m_wb_cyc_o ( from_m_wb1_cyc_o ),
+    .from_m_wb_stb_o ( from_m_wb1_stb_o ),
+    .to_m_wb_ack_i   ( to_m_wb1_ack_i   ),
+    .to_m_wb_err_i   ( to_m_wb1_err_i   ),
+    .to_m_wb_dat_i   ( to_m_wb1_dat_i   ),
+
+    .from_m_wb_cti_o ( from_m_wb1_cti_o ),
+    .from_m_wb_bte_o ( from_m_wb1_bte_o ),
+
+    //// to ahb3lite
+    .mHSEL           ( m1HSEL           ),
+    .mHSIZE          ( m1HSIZE          ),
+    .mHRDATA         ( m1HRDATA         ),
+    .mHRESP          ( m1HRESP          ),
+    .mHREADY         ( m1HREADY         ),
+    .mHREADYOUT      ( m1HREADYOUT      ),
+    .mHWRITE         ( m1HWRITE         ),
+    .mHBURST         ( m1HBURST         ),
+    .mHADDR          ( m1HADDR          ),
+    .mHTRANS         ( m1HTRANS         ),
+    .mHWDATA         ( m1HWDATA         ),
+    .mHPROT          ( m1HPROT          ) );
+
+
+  ahb3lite_dma #(
+    // chXX_conf = { CBUF, ED, ARS, EN }
+    .rf_addr     ( rf_addr          ),
+    .pri_sel     ( pri_sel          ),
+    .ch_count    ( ch_count         ),
+    .ch0_conf    ( ch0_conf         ),
+    .ch1_conf    ( ch1_conf         ),
+    .ch2_conf    ( ch2_conf         ),
+    .ch3_conf    ( ch3_conf         ),
+    .ch4_conf    ( ch4_conf         ),
+    .ch5_conf    ( ch5_conf         ),
+    .ch6_conf    ( ch6_conf         ),
+    .ch7_conf    ( ch7_conf         ),
+    .ch8_conf    ( ch8_conf         ),
+    .ch9_conf    ( ch9_conf         ),
+    .ch10_conf   ( ch10_conf        ),
+    .ch11_conf   ( ch11_conf        ),
+    .ch12_conf   ( ch12_conf        ),
+    .ch13_conf   ( ch13_conf        ),
+    .ch14_conf   ( ch14_conf        ),
+    .ch15_conf   ( ch15_conf        ),
+    .ch16_conf   ( ch16_conf        ),
+    .ch17_conf   ( ch17_conf        ),
+    .ch18_conf   ( ch18_conf        ),
+    .ch19_conf   ( ch19_conf        ),
+    .ch20_conf   ( ch20_conf        ),
+    .ch21_conf   ( ch21_conf        ),
+    .ch22_conf   ( ch22_conf        ),
+    .ch23_conf   ( ch23_conf        ),
+    .ch24_conf   ( ch24_conf        ),
+    .ch25_conf   ( ch25_conf        ),
+    .ch26_conf   ( ch26_conf        ),
+    .ch27_conf   ( ch27_conf        ),
+    .ch28_conf   ( ch28_conf        ),
+    .ch29_conf   ( ch29_conf        ),
+    .ch30_conf   ( ch30_conf        ) )
+  ahb3lite_dma0 (
+    // Common signals
+    .clk_i       ( clk_i            ),
+    .rst_i       ( rst_i            ),
+
+    // --------------------------------------
+    // WISHBONE INTERFACE 0
+    // Slave Interface
+    .wb0s_data_i ( to_wb0_dat_i     ),
+    .wb0s_data_o ( from_wb0_dat_o   ),
+    .wb0_addr_i  ( to_wb0_adr_i     ),
+    .wb0_sel_i   ( to_wb0_sel_i     ),
+    .wb0_we_i    ( to_wb0_we_i      ),
+    .wb0_cyc_i   ( to_wb0_cyc_i     ),
+    .wb0_stb_i   ( to_wb0_stb_i     ),
+    .wb0_ack_o   ( from_wb0_ack_o   ),
+    .wb0_err_o   ( from_wb0_err_o   ),
+    .wb0_rty_o   (                  ),
+
+    // Master Interface
+    .wb0m_data_i ( to_m_wb0_dat_i   ),
+    .wb0m_data_o ( from_m_wb0_dat_o ),
+    .wb0_addr_o  ( from_m_wb0_adr_o ),
+    .wb0_sel_o   ( from_m_wb0_sel_o ),
+    .wb0_we_o    ( from_m_wb0_we_o  ),
+    .wb0_cyc_o   ( from_m_wb0_cyc_o ),
+    .wb0_stb_o   ( from_m_wb0_stb_o ),
+    .wb0_ack_i   ( to_m_wb0_ack_i   ),
+    .wb0_err_i   ( to_m_wb0_err_i   ),
+    .wb0_rty_i   ( 1'b0             ),
+
+    // --------------------------------------
+    // WISHBONE INTERFACE 1
+    // Slave Interface
+    .wb1s_data_i ( to_wb1_dat_i     ),
+    .wb1s_data_o ( from_wb1_dat_o   ),
+    .wb1_addr_i  ( to_wb1_adr_i     ),
+    .wb1_sel_i   ( to_wb1_sel_i     ),
+    .wb1_we_i    ( to_wb1_we_i      ),
+    .wb1_cyc_i   ( to_wb1_cyc_i     ),
+    .wb1_stb_i   ( to_wb1_stb_i     ),
+    .wb1_ack_o   ( from_wb1_ack_o   ),
+    .wb1_err_o   ( from_wb1_err_o   ),
+    .wb1_rty_o   (                  ),
+
+    // Master Interface
+    .wb1m_data_i ( to_m_wb0_dat_i   ),
+    .wb1m_data_o ( from_m_wb0_dat_o ),
+    .wb1_addr_o  ( from_m_wb0_adr_o ),
+    .wb1_sel_o   ( from_m_wb0_sel_o ),
+    .wb1_we_o    ( from_m_wb0_we_o  ),
+    .wb1_cyc_o   ( from_m_wb0_cyc_o ),
+    .wb1_stb_o   ( from_m_wb0_stb_o ),
+    .wb1_ack_i   ( to_m_wb0_ack_i   ),
+    .wb1_err_i   ( to_m_wb0_err_i   ),
+    .wb1_rty_i   ( 1'b0             ),
+
+    // --------------------------------------
+    // Misc Signals
+    .dma_req_i   ( dma_req_i        ),
+    .dma_nd_i    ( dma_nd_i         ),
+    .dma_ack_o   ( dma_ack_o        ),
+    .dma_rest_i  ( dma_rest_i       ),
+    .inta_o      ( inta_o           ),
+    .intb_       ( intb_o           ) );
